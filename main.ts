e00fe205bc29e7da13f350e33a1bdef412038354
martinmcy <104663090+martinmcy@users.noreply.github.com>
2022-06-04 12:14:31 +0800

    2

--- main.ts
+++ main.ts
@@ -1,12 +1,70 @@
 function stop () {
-	
+    sensors.DDMmotor(
+    AnalogPin.P13,
+    0,
+    AnalogPin.P14,
+    0
+    )
+    sensors.DDMmotor(
+    AnalogPin.P15,
+    0,
+    AnalogPin.P16,
+    0
+    )
 }
+let on = 0
 let item = 0
 let anglre = 90
+stop()
 basic.forever(function () {
     if (input.buttonIsPressed(Button.A)) {
         anglre += 10
         pins.servoWritePin(AnalogPin.P1, anglre)
         basic.pause(50)
+    }
+    if (input.buttonIsPressed(Button.B)) {
+        anglre += -10
+        pins.servoWritePin(AnalogPin.P1, anglre)
+        basic.pause(50)
+    }
+    if (pins.digitalReadPin(DigitalPin.P0) == 0) {
+        stop()
+        if (item < 1) {
+            anglre += 1
+            basic.showIcon(IconNames.Giraffe)
+        } else {
+            anglre = 0
+            basic.showIcon(IconNames.Square)
+        }
+        basic.pause(500)
+    }
+})
+basic.forever(function () {
+    if (item == 0) {
+        if (pins.digitalReadPin(DigitalPin.P8) == 0) {
+            if (on == 0) {
+                on += 1
+            } else {
+                on = 0
+                stop()
+            }
+        }
+        if (pins.digitalReadPin(DigitalPin.P12) == 0) {
+            if (on == 0) {
+                on += 1
+            } else {
+                on = 0
+                stop()
+            }
+        }
+        basic.pause(300)
+    } else if (item == 1) {
+        if (pins.digitalReadPin(DigitalPin.P8) == 1) {
+        	
+        } else if (pins.digitalReadPin(DigitalPin.P12) == 1) {
+        	
+        } else {
+            stop()
+        }
     }
 })
