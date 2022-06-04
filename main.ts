241452014630d4757bb5690d5879d04e50372356
martinmcy <104663090+martinmcy@users.noreply.github.com>
2022-06-04 12:01:20 +0800

    1

--- main.ts
+++ main.ts
@@ -1,1 +1,12 @@
-
+function stop () {
+	
+}
+let item = 0
+let anglre = 90
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        anglre += 10
+        pins.servoWritePin(AnalogPin.P1, anglre)
+        basic.pause(50)
+    }
+})
