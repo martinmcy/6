030a1354c53a80e335081d5f44570c38fbea8f28
martinmcy <104663090+martinmcy@users.noreply.github.com>
2022-06-18 09:05:10 +0800

    3

--- main.ts
+++ main.ts
@@ -17,6 +17,57 @@ let item = 0
 let anglre = 90
 stop()
 basic.forever(function () {
+    if (item == 0) {
+        if (pins.digitalReadPin(DigitalPin.P8) == 0) {
+            if (on == 0) {
+                on += 1
+                sensors.DDMmotor(
+                AnalogPin.P13,
+                0,
+                AnalogPin.P14,
+                60
+                )
+            } else {
+                on = 0
+                stop()
+            }
+        }
+        if (pins.digitalReadPin(DigitalPin.P12) == 0) {
+            if (on == 0) {
+                on += 1
+                sensors.DDMmotor(
+                AnalogPin.P13,
+                0,
+                AnalogPin.P14,
+                60
+                )
+            } else {
+                on = 0
+                stop()
+            }
+        }
+        basic.pause(300)
+    } else if (item == 1) {
+        if (pins.digitalReadPin(DigitalPin.P8) == 1) {
+            sensors.DDMmotor(
+            AnalogPin.P15,
+            0,
+            AnalogPin.P16,
+            80
+            )
+        } else if (pins.digitalReadPin(DigitalPin.P12) == 1) {
+            sensors.DDMmotor(
+            AnalogPin.P15,
+            1,
+            AnalogPin.P16,
+            80
+            )
+        } else {
+            stop()
+        }
+    }
+})
+basic.forever(function () {
     if (input.buttonIsPressed(Button.A)) {
         anglre += 10
         pins.servoWritePin(AnalogPin.P1, anglre)
@@ -37,34 +88,5 @@ basic.forever(function () {
             basic.showIcon(IconNames.Square)
         }
         basic.pause(500)
-    }
-})
-basic.forever(function () {
-    if (item == 0) {
-        if (pins.digitalReadPin(DigitalPin.P8) == 0) {
-            if (on == 0) {
-                on += 1
-            } else {
-                on = 0
-                stop()
-            }
-        }
-        if (pins.digitalReadPin(DigitalPin.P12) == 0) {
-            if (on == 0) {
-                on += 1
-            } else {
-                on = 0
-                stop()
-            }
-        }
-        basic.pause(300)
-    } else if (item == 1) {
-        if (pins.digitalReadPin(DigitalPin.P8) == 1) {
-        	
-        } else if (pins.digitalReadPin(DigitalPin.P12) == 1) {
-        	
-        } else {
-            stop()
-        }
     }
 })
